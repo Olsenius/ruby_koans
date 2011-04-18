561ff7b3a6a04830abe25d0042ed471e823a82e1
Andreas Lien Olsen <andreas.olsen@gmail.com>
2011-04-18 21:08:19 +0200

    Made tests pass

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,10 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if (a<0 || b<0 || c<0)
+    raise TriangleError
+  end
   if (a==b && a ==c)
+    if (a==0)
+      raise TriangleError
+    end
     return :equilateral
   else
     if (a==b || a==c || b==c)
+      numbers = [a, b, c].sort
+
+      if (numbers[0]+numbers[1] < numbers[2])
+        raise TriangleError
+      elsif (numbers[0]+numbers[1] == numbers[2])
+        raise TriangleError
+      end
+
       return :isosceles
     end
   end
