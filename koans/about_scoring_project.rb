3637f71c012a4ca7325295665a48ee19c86adf60
Andreas Lien Olsen <andreas.olsen@gmail.com>
2011-04-21 00:56:43 +0200

    Phew. This one took some time. Strange to work in ruby even with this simple logic:-)

--- koans/about_scoring_project.rb
+++ koans/about_scoring_project.rb
@@ -29,8 +29,34 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #
 # Your goal is to write the score method.
 
-def score(dice)
-  # You need to write this method
+def score(dices)
+
+  counters = Array.new(7)
+  1.upto 6 do |index|
+    counters[index]=0
+  end
+
+  dices.each do |dice|
+    counters[dice]+=1
+  end
+
+  sum = counters[1] * 100
+  sum += counters[5] * 50
+
+  1.upto 6 do |index|
+    counter = counters[index]
+    if counter >= 3
+      sum += (index * 100)
+
+      if index == 1
+        sum +=600
+        end
+      if index == 5
+        sum -=150
+      end
+    end
+  end
+  return sum
 end
 
 class AboutScoringProject < EdgeCase::Koan
@@ -47,28 +73,28 @@ class AboutScoringProject < EdgeCase::Koan
   end
 
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
+    assert_equal 300, score([1, 5, 5, 1])
   end
 
   def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
+    assert_equal 0, score([2, 3, 4, 6])
   end
 
   def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
+    assert_equal 1000, score([1, 1, 1])
   end
 
   def test_score_of_other_triples_is_100x
-    assert_equal 200, score([2,2,2])
-    assert_equal 300, score([3,3,3])
-    assert_equal 400, score([4,4,4])
-    assert_equal 500, score([5,5,5])
-    assert_equal 600, score([6,6,6])
+    assert_equal 200, score([2, 2, 2])
+    assert_equal 300, score([3, 3, 3])
+    assert_equal 400, score([4, 4, 4])
+    assert_equal 500, score([5, 5, 5])
+    assert_equal 600, score([6, 6, 6])
   end
 
   def test_score_of_mixed_is_sum
-    assert_equal 250, score([2,5,2,2,3])
-    assert_equal 550, score([5,5,5,5])
+    assert_equal 250, score([2, 5, 2, 2, 3])
+    assert_equal 550, score([5, 5, 5, 5])
   end
 
 end
